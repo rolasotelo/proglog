5fce07b977b8bc466c150b1831401b286ab56b45
Rolando Sotelo <rola@hey.com>
2022-04-17 03:27:36 -0500

    feat: implement grpc server

--- internal/log/log.go
+++ internal/log/log.go
@@ -1,7 +1,6 @@
 package log
 
 import (
-	"fmt"
 	api "github.com/rolasotelo/proglog/api/v1"
 	"io"
 	"io/ioutil"
@@ -94,7 +93,7 @@ func (l *Log) Read(off uint64) (*api.Record, error) {
 		}
 	}
 	if s == nil || s.nextOffset <= off {
-		return nil, fmt.Errorf("offset out of range: %d", off)
+		return nil, api.ErrOffsetOutOfRange{Offset: off}
 	}
 	return s.Read(off)
 }
